7cf87b94dbdc11976219bc32a3b441bd7c9f2b79
p17leka <p17leka@ionio.gr>
2020-05-27 15:14:02 +0300

    Update html-processor.py

--- html-processor.py
+++ html-processor.py
@@ -1,7 +1,6 @@
 import re
 
-#function used to turn html special entities into something else according to ta
-ble
+#function used to turn html special entities into something else according to table
 
 def function(i):
         if(i.group(0) == "&amp;"):
@@ -13,16 +12,11 @@ def function(i):
         elif (i.group(0) == "&nbsp;"):
                 return ' '
 
-
 rexp1 = re.compile(r'<title>(.+?)</title>') #export title process 1 thats asked
-rexp2 = re.compile(r'<!--.*?-->',re.DOTALL) #export comments process 2 thats ask
-ed 
-rexp3 = re.compile(r'<(script|style).*?>.*?</(script|style)>',re.DOTALL) #export
- script and style tags with their content process 3 thats asked 
-rexp4 = re.compile(r'<a.+?href="(.*?)".*?>(.*?)</a>',re.DOTALL) #export hrefs-li
-nks process 4 thats asked
-rexp5part1 = re.compile(r'<.+?/>',re.DOTALL) #export tags like <..../> process 5
- thats asked
+rexp2 = re.compile(r'<!--.*?-->',re.DOTALL) #export comments process 2 thats asked 
+rexp3 = re.compile(r'<(script|style).*?>.*?</(script|style)>',re.DOTALL) #export script and style tags with their content process 3 thats asked 
+rexp4 = re.compile(r'<a.+?href="(.*?)".*?>(.*?)</a>',re.DOTALL) #export hrefs-links process 4 thats asked
+rexp5part1 = re.compile(r'<.+?/>',re.DOTALL) #export tags like <..../> process 5 thats asked
 rexp5part2 = re.compile(r'<.+?>|</.+?>',re.DOTALL) #export tags like <...>...<..../> also process 5 thats asked
 rexp6 = re.compile(r'&(amp|gt|lt|nbsp)') #export html special entities according to table presented process 6 thats asked
 rexp7 = re.compile(r'\s+') #export continuous whitespace characters process 7 thats asked
